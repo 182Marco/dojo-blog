9a69028d29a67ec00ae33d57e7c3236e1255b4a5
182Marco <marcomilza@gmail.com>
2021-04-30 15:16:16 +0200

    altri esperimenti fatti

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './navbar';
 import Navbar from './navbar';
 import Home from './Home';
+import { useState } from 'react';
 
 function App() {
-  const title = `Welcome to the new blog`;
+  const [nomeTizo, set] = useState('Ugo');
+  function cambiaNome() {
+    nomeTizo == 'Ugo' ? set('Edo') : set('Ugo');
+  }
   const likes = 50;
   const link = `http://www.google.com`;
   return (
@@ -13,7 +17,13 @@ function App() {
         <Home />
         <p> like: {likes} times</p>
         <a href={link}>link alla pagina di google</a>
+        <h1>{nomeTizo}</h1>
+        <button onClick={cambiaNome}>
+          Chiama la cazzo di funzione per cambiare il nome sopra
+        </button>
       </div>
     </div>
   );
 }
+
+export default App;
