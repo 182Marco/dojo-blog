d6daddca99c9224f61bcea43d4e147debada2628
182Marco <marcomilza@gmail.com>
2021-06-29 22:32:56 +0200

    feat. styles transported in scss

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Home from './Home';
 
 function App() {
   const likes = 50;
-  const link = `http://www.google.com`;
   return (
     <div className='App'>
       <Navbar />
       <div className='content'>
         <Home />
         <p> like: {likes} times</p>
-        <a href={link}>link alla pagina di google</a>
       </div>
     </div>
   );
--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,11 @@
+const GogLink = `http://www.google.com`;
+
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <h1>Il blog della grande poesia</h1>
       <div className='links'>
-        <a href=''>Home</a>
+        <a href={GogLink}>Home google</a>
         <a href=''>New Blog</a>
       </div>
     </nav>
