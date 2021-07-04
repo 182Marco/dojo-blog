ca4ae546910361fa296c395c4e52af5fa12468e9
182Marco <marcomilza@gmail.com>
2021-07-04 18:34:53 +0200

    fix.Lines of poetry now displayed in different lines

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -5,10 +5,15 @@ const Bloglist = ({ blogs, title, handleDelete, setBlogs }) => {
   return (
     <div className='blog-preview'>
       <h2>{title}</h2>
-      {blogs.map(blog => (
-        <div className='blog-preview' key={blog.id}>
+      {blogs.map((blog, i) => (
+        <div className='blog-preview' key={i}>
           <h3 className='artTitle'>{blog.title}</h3>
-          <p>{blog.body}</p>
+          {/* map inside map to break all lines of each
+          poetry inside a single p line */}
+          {blog.body.split('\n').map((line, i) => {
+            return <p key={i}>{line}</p>;
+          })}
+          {/* ---- */}
           <p className='author'>
             Scritto da: <br /> <em> {blog.author} </em>
           </p>
