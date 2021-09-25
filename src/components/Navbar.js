168a696a63323fd0f8ddfd4523bd86b10ca40f0f
182Marco <marcomilza@gmail.com>
2021-09-26 01:50:04 +0200

    fit.created class active for nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  // VARIABILES
+  //assigning location variable
+  const location = useLocation();
+  //destructuring pathname from location
+  const { pathname } = location;
+  //Javascript split method to get the name of the path in array
+  const splitLocation = pathname.split('/');
+
   return (
     <nav className='navbar content'>
       <h1>Il blog della grande poesia</h1>
       <ul className='links'>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to='/' className={splitLocation[1] === '' ? 'active' : ''}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to='/Create-New-Blog'>Create</Link>
+          <Link
+            to='/Create-New-Blog'
+            className={splitLocation[1] === 'Create-New-Blog' ? 'active' : ''}
+          >
+            Create
+          </Link>
         </li>
       </ul>
     </nav>
